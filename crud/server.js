9309bb1e91d4ae99c7899388bb6616309d990d5e
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-10-15 22:06:41 +0530

    some packages like json and cookies parser installed and initializr it in server

--- crud/server.js
+++ crud/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { initializeDB } from "./db.js";
 import userRoute from "./routes/userRoute.js"
+import cookieParser from "cookie-parser";
 
+
+// this is for database connection initialize
 initializeDB();
+
 
 const app = express();
 
+// This is Middleware to parse incoming JOSN data
 app.use(express.json());
+
+// THis is Middleware for to parse cookies
+app.use(cookieParser());
 
 // Simple route for Testing purpose
 app.get("/", (req, res) => {
