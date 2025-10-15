85d5b23d630853f97a1abd7af53f6c2c3ab1ff51
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-10-15 22:05:52 +0530

    add route for login and home page

--- crud/routes/userRoute.js
+++ crud/routes/userRoute.js
@@ -1,7 +1,21 @@
 import express from 'express';
-import { registerController } from '../controllers/userController.js';
+import { loginController, registerController } from '../controllers/userController.js';
+import { authMiddleware} from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/register',registerController);
+
+router.post('/login',loginController);
+
+router.get("/",authMiddleware,(req,res) => {
+    const data = req.user;
+    console.log(data);
+
+    return res.status(200).json({
+        success:true,
+        messsage: 'User can login and we idetify what is role of user',
+        data,
+    })
+})
 
 export default router;
